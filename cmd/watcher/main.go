8d45ae07e22ff66edfa47c228b38b7a466e99004
Gleb <gleb.beloded@gmail.com>
2020-04-29 23:17:23 +0300

    zone name prefix added for better logging

--- cmd/watcher/main.go
+++ cmd/watcher/main.go
@@ -10,16 +10,18 @@ import (
 	types "github.com/mapofzones/cosmos-watcher/types"
 )
 
-var tendermintRPC, rabbitmqAddr *string
+var tendermintRPC, rabbitmqAddr, zoneName *string
 
 func init() {
 	tendermintRPC = flag.String("tmRPC", "tcp://localhost:26657", "address of tendermint node to connect to")
 	rabbitmqAddr = flag.String("rabbitMQ", "", "rabbitmq url to connect to")
+	zoneName = flag.String("zone", "", "zone name to be displayed by logger")
 }
 
 func main() {
 	// log raw data without time and date prefixes
 	log.SetFlags(0)
+	log.SetPrefix(*zoneName)
 	// parse flags from stdin
 	flag.Parse()
 	run()
