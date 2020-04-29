53743b376a024b31117aab9d202d1303cfcea2d9
Gleb <gleb.beloded@gmail.com>
2020-04-29 18:55:03 +0300

    run all script fix

--- run_all.sh
+++ run_all.sh
@@ -2,7 +2,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 for filename in $DIR/configs/*.json; do
-    watcher --tmRPC $(jq <$filename .NodeAddr -r) --rabbitMQ $RABBITMQ
+    watcher --tmRPC $(jq <$filename .NodeAddr -r) --rabbitMQ $RABBITMQ &
 done
 
 wait
