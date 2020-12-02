b69d130a8594f7e77af5e98c6981fb1156a0ee77
Raumo0 <dharapko@gmail.com>
2020-12-02 18:40:46 +0300

    run script update

--- scripts/run.sh
+++ scripts/run.sh
@@ -12,18 +12,23 @@ if [ "$chain_id" == "" ]; then
     exit 1
 fi
 
-height=$(curl -H 'Content-Type: application/json' \
- -X POST -ss -H "x-hasura-admin-secret: $hasura_secret" \
- --data '{"query":"{blocks_log(where: {zone: {_eq: \"'"$chain_id"'\"}}) {last_processed_block}}"}' $graphql \
-  | jq .data.blocks_log[].last_processed_block)
+new_height=""
 
-if [ "$height" == "" ]; then
-    height="$height"
-    if [ "$height" == "" ]; then
-    height=0
+if [ "$graphql" != "" ]; then
+    height=$(curl -H 'Content-Type: application/json' \
+     -X POST -ss -H "x-hasura-admin-secret: $hasura_secret" \
+     --data '{"query":"{blocks_log(where: {zone: {_eq: \"'"$chain_id"'\"}}) {last_processed_block}}"}' $graphql \
+      | jq .data.blocks_log[].last_processed_block)
+fi
+
+if [ "$new_height" == "" ]; then
+    new_height="$height"
+    if [ "$new_height" == "" ]; then
+        new_height=0
     fi
 fi
 
+height="$new_height"
 # increment height since we need to start getting blocks from last_processed_height +1
 let "height=height+1"
 export height
