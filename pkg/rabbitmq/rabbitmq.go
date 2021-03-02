178c127a632492f9dba29459b4136836fdfc1beb
Ildar Nurmukhametov <neodiz@gmail.com>
2021-03-02 10:49:25 +0300

    Add finish notification then publish block

--- pkg/rabbitmq/rabbitmq.go
+++ pkg/rabbitmq/rabbitmq.go
@@ -78,22 +78,24 @@ func BlockQueue(ctx context.Context, addr string, queue string) (chan<- watcher.
 				}
 				go func() {
 				// now wait for confirmation in order to preserve order
-				select {
-				case <-time.After(time.Second * 5):
-					log.Println("Timeout notification from rabbit")
-				case confirmation := <-notifications:
-					if !confirmation.Ack {
-						//server could not receive our  publishing
-						log.Println("rabbitmq err not ack")
+				for {
+					select {
+					case <-time.After(3 * time.Second):
+						log.Println("Timeout notification from rabbit")
+					case confirmation := <-notifications:
+						if !confirmation.Ack {
+							//server could not receive our  publishing
+							log.Println("rabbitmq err not ack")
+							close(blockStream)
+							return
+						} else {
+							log.Println("Finished publish block= ", block.Height())
+						}
+					case <-ctx.Done():
+						log.Println("rabbitmq ctx done")
 						close(blockStream)
 						return
-					} else {
-						log.Println("Finished publish block= ",block.Height())
 					}
-				case <-ctx.Done():
-					log.Println("rabbitmq ctx done")
-					close(blockStream)
-					return
 				}
 				}()
 
