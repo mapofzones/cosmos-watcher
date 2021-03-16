223762bf5742fe1f85f35e3e5367b11432125bd8
Raumo0 <dharapko@gmail.com>
2021-03-16 08:22:15 +0300

    Added cosmos codec msgs

--- pkg/codec/protobuf_codec.go
+++ pkg/codec/protobuf_codec.go
@@ -3,9 +3,19 @@ package watcher
 import (
 	cosmoscodectypes "github.com/cosmos/cosmos-sdk/codec/types"
 	cosmostypes "github.com/cosmos/cosmos-sdk/types"
+	cosmosauthvestingtypes "github.com/cosmos/cosmos-sdk/x/auth/vesting/types"
 	cosmosbanktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
-	cosmosibctypes "github.com/cosmos/cosmos-sdk/x/ibc/core/02-client/types"
+	cosmoscrisistypes "github.com/cosmos/cosmos-sdk/x/crisis/types"
+	cosmosdistributiontypes "github.com/cosmos/cosmos-sdk/x/distribution/types"
+	cosmosevidencetypes "github.com/cosmos/cosmos-sdk/x/evidence/types"
+	cosmosgovtypes "github.com/cosmos/cosmos-sdk/x/gov/types"
+	cosmosibcapptransfertypes "github.com/cosmos/cosmos-sdk/x/ibc/applications/transfer/types"
+	cosmosibcclienttypes "github.com/cosmos/cosmos-sdk/x/ibc/core/02-client/types"
+	cosmosibcconnectiontypes "github.com/cosmos/cosmos-sdk/x/ibc/core/03-connection/types"
+	cosmosibcchanneltypes "github.com/cosmos/cosmos-sdk/x/ibc/core/04-channel/types"
+	cosmosslashingtypes "github.com/cosmos/cosmos-sdk/x/slashing/types"
 	cosmosstakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
+
 	cybergraphtypes "github.com/cybercongress/go-cyber/x/graph/types"
 	cyberrecourcestypes "github.com/cybercongress/go-cyber/x/resources/types"
 	"github.com/gogo/protobuf/proto"
@@ -22,7 +32,7 @@ func RegisterMessagesImplementations(interfaceRegistry cosmoscodectypes.Interfac
 }
 
 func getMessageImplementations() []proto.Message {
-	impls := []proto.Message{}
+	var impls []proto.Message
 
 	cosmosMessages := getCosmosMessages()
 	cyberMessages := getCyberMessages()
@@ -30,14 +40,50 @@ func getMessageImplementations() []proto.Message {
 	impls = append(impls, cosmosMessages...)
 	impls = append(impls, cyberMessages...)
 	impls = append(impls, akashMessages...)
+
 	return impls
 }
 
 func getCosmosMessages() []proto.Message {
 	cosmosMessages := []proto.Message{
 		&cosmosbanktypes.MsgSend{},
-		&cosmosibctypes.MsgCreateClient{},
+		&cosmosibcclienttypes.MsgCreateClient{},
 		&cosmosstakingtypes.MsgCreateValidator{},
+		&cosmosibcchanneltypes.MsgAcknowledgement{},
+		&cosmosstakingtypes.MsgBeginRedelegate{},
+		&cosmosibcchanneltypes.MsgChannelCloseConfirm{},
+		&cosmosibcchanneltypes.MsgChannelCloseInit{},
+		&cosmosibcchanneltypes.MsgChannelOpenAck{},
+		&cosmosibcchanneltypes.MsgChannelOpenConfirm{},
+		&cosmosibcchanneltypes.MsgChannelOpenInit{},
+		&cosmosibcchanneltypes.MsgChannelOpenTry{},
+		&cosmosibcconnectiontypes.MsgConnectionOpenAck{},
+		&cosmosibcconnectiontypes.MsgConnectionOpenConfirm{},
+		&cosmosibcconnectiontypes.MsgConnectionOpenInit{},
+		&cosmosibcconnectiontypes.MsgConnectionOpenTry{},
+		&cosmosauthvestingtypes.MsgCreateVestingAccount{},
+		&cosmosstakingtypes.MsgDelegate{},
+		&cosmosgovtypes.MsgDeposit{},
+		&cosmosstakingtypes.MsgEditValidator{},
+		&cosmosdistributiontypes.MsgFundCommunityPool{},
+		&cosmosbanktypes.MsgMultiSend{},
+		&cosmosibcchanneltypes.MsgRecvPacket{},
+		&cosmosdistributiontypes.MsgSetWithdrawAddress{},
+		&cosmosevidencetypes.MsgSubmitEvidence{},
+		&cosmosibcclienttypes.MsgSubmitMisbehaviour{},
+		&cosmosgovtypes.MsgSubmitProposal{},
+		&cosmosibcchanneltypes.MsgTimeout{},
+		&cosmosibcchanneltypes.MsgTimeoutOnClose{},
+		&cosmosibcapptransfertypes.MsgTransfer{},
+		&cosmosstakingtypes.MsgUndelegate{},
+		&cosmosslashingtypes.MsgUnjail{},
+		&cosmosibcclienttypes.MsgUpdateClient{},
+		&cosmosibcclienttypes.MsgUpgradeClient{},
+		&cosmoscrisistypes.MsgVerifyInvariant{},
+		&cosmosgovtypes.MsgVote{},
+		&cosmosdistributiontypes.MsgWithdrawDelegatorReward{},
+		&cosmosdistributiontypes.MsgWithdrawValidatorCommission{},
+		&cosmostypes.ServiceMsg{},
 	}
 	return cosmosMessages
 }
