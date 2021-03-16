7b5442a30a2817ccff087f500c1bac8b64ef626d
Raumo0 <dharapko@gmail.com>
2021-03-17 02:59:54 +0300

    Removed duplication cosmoc codec config

--- pkg/codec/protobuf_codec.go
+++ pkg/codec/protobuf_codec.go
@@ -4,19 +4,8 @@ import (
 	"github.com/gogo/protobuf/proto"
 
 	cosmoscodectypes "github.com/cosmos/cosmos-sdk/codec/types"
+	cosmossimapp "github.com/cosmos/cosmos-sdk/simapp"
 	cosmostypes "github.com/cosmos/cosmos-sdk/types"
-	cosmosauthvestingtypes "github.com/cosmos/cosmos-sdk/x/auth/vesting/types"
-	cosmosbanktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
-	cosmoscrisistypes "github.com/cosmos/cosmos-sdk/x/crisis/types"
-	cosmosdistributiontypes "github.com/cosmos/cosmos-sdk/x/distribution/types"
-	cosmosevidencetypes "github.com/cosmos/cosmos-sdk/x/evidence/types"
-	cosmosgovtypes "github.com/cosmos/cosmos-sdk/x/gov/types"
-	cosmosibcapptransfertypes "github.com/cosmos/cosmos-sdk/x/ibc/applications/transfer/types"
-	cosmosibcclienttypes "github.com/cosmos/cosmos-sdk/x/ibc/core/02-client/types"
-	cosmosibcconnectiontypes "github.com/cosmos/cosmos-sdk/x/ibc/core/03-connection/types"
-	cosmosibcchanneltypes "github.com/cosmos/cosmos-sdk/x/ibc/core/04-channel/types"
-	cosmosslashingtypes "github.com/cosmos/cosmos-sdk/x/slashing/types"
-	cosmosstakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
 
 	//cyberapp "github.com/cybercongress/go-cyber/app"
 	cybercrontypes "github.com/cybercongress/go-cyber/x/cron/types"
@@ -41,19 +30,7 @@ func RegisterMessagesImplementations(interfaceRegistry cosmoscodectypes.Interfac
 }
 
 func registerCosmosInterfaces(interfaceRegistry cosmoscodectypes.InterfaceRegistry) {
-	cosmosibcchanneltypes.RegisterInterfaces(interfaceRegistry)
-	cosmosbanktypes.RegisterInterfaces(interfaceRegistry)
-	cosmosibcclienttypes.RegisterInterfaces(interfaceRegistry)
-	cosmosstakingtypes.RegisterInterfaces(interfaceRegistry)
-	cosmosibcconnectiontypes.RegisterInterfaces(interfaceRegistry)
-	cosmosauthvestingtypes.RegisterInterfaces(interfaceRegistry)
-	cosmosgovtypes.RegisterInterfaces(interfaceRegistry)
-	cosmosdistributiontypes.RegisterInterfaces(interfaceRegistry)
-	cosmosevidencetypes.RegisterInterfaces(interfaceRegistry)
-	cosmosibcapptransfertypes.RegisterInterfaces(interfaceRegistry)
-	cosmosslashingtypes.RegisterInterfaces(interfaceRegistry)
-	cosmoscrisistypes.RegisterInterfaces(interfaceRegistry)
-	cosmostypes.RegisterInterfaces(interfaceRegistry)
+	cosmossimapp.ModuleBasics.RegisterInterfaces(interfaceRegistry)
 }
 
 func registerCyberInterfaces(interfaceRegistry cosmoscodectypes.InterfaceRegistry) {
