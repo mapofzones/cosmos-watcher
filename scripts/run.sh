c433a8e7d0be738d73ba2c7609a6d284111c3d37
Raumo0 <dharapko@gmail.com>
2022-02-04 17:20:38 +0100

    MOZ-238 get rpc query in initial script improved

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,9 +1,31 @@
 #!/bin/bash
 
+new_height=""
+
+if [ "$graphql" != "" ]; then
+    height=$(curl -H 'Content-Type: application/json' \
+     -X POST -ss -H "x-hasura-admin-secret: $hasura_secret" \
+     --data '{"query":"{blocks_log(where: {zone: {_eq: \"'"$chain_id"'\"}}) {last_processed_block}}"}' $graphql \
+      | jq .data.blocks_log[].last_processed_block)
+fi
+
+if [ "$new_height" == "" ]; then
+    new_height="$height"
+    if [ "$new_height" == "" ]; then
+        new_height=0
+    fi
+fi
+
+height="$new_height"
+# increment height since we need to start getting blocks from last_processed_height +1
+let "height=height+1"
+export height
+
+
 if [ "$chain_id" != "" ]; then
     resp=$(curl -H 'Content-Type: application/json' \
      -X POST -ss -H "x-hasura-admin-secret: $hasura_secret" \
-     --data '{"query":"{zone_nodes(where: {zone: {_eq: \"'"$chain_id"'\"}, is_alive: {_eq: true}}, order_by: {last_checked_at: desc}) {rpc_addr}}"}' $graphql \
+     --data '{"query":"{zone_nodes(where: {zone: {_eq: \"'"$chain_id"'\"}, is_alive: {_eq: true}, tx_index: {_eq: \"on\"}, last_block_height: {_gt: '"$new_height"'}, earliest_block_height: {_lte: '"$new_height"'}}, order_by: {last_checked_at: desc}) {rpc_addr}}"}' $graphql \
       | jq .data.zone_nodes[].rpc_addr)
 
     x=0
@@ -27,27 +49,6 @@ if [ "$rpc" == "" ]; then
     sleep 600
     exit 1
 fi
-
-new_height=""
-
-if [ "$graphql" != "" ]; then
-    height=$(curl -H 'Content-Type: application/json' \
-     -X POST -ss -H "x-hasura-admin-secret: $hasura_secret" \
-     --data '{"query":"{blocks_log(where: {zone: {_eq: \"'"$chain_id"'\"}}) {last_processed_block}}"}' $graphql \
-      | jq .data.blocks_log[].last_processed_block)
-fi
-
-if [ "$new_height" == "" ]; then
-    new_height="$height"
-    if [ "$new_height" == "" ]; then
-        new_height=0
-    fi
-fi
-
-height="$new_height"
-# increment height since we need to start getting blocks from last_processed_height +1
-let "height=height+1"
-export height
 
 echo "starting watcher for $chain_id on $rpc at height: $height"
 /app/watcher ; sleep 60
