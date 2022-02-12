343f7b22865fe18ba23efb009c642e87ac85d65e
Raumo0 <dharapko@gmail.com>
2022-02-12 09:02:52 +0100

    Stargaze codec & dependencies upgrade

--- pkg/codec/protobuf_codec.go
+++ pkg/codec/protobuf_codec.go
@@ -9,9 +9,9 @@ import (
 	cosmoscryptosecp "github.com/cosmos/cosmos-sdk/crypto/keys/secp256k1"
 	cosmoscryptotypes "github.com/cosmos/cosmos-sdk/crypto/types"
 	cosmostypes "github.com/cosmos/cosmos-sdk/types"
-	ibcexported "github.com/cosmos/ibc-go/modules/core/exported"
-	ibcclients "github.com/cosmos/ibc-go/modules/light-clients/07-tendermint/types"
-	stargazeapp "github.com/public-awesome/stargaze/app"
+	ibcexported "github.com/cosmos/ibc-go/v2/modules/core/exported"
+	ibcclients "github.com/cosmos/ibc-go/v2/modules/light-clients/07-tendermint/types"
+	stargazeapp "github.com/public-awesome/stargaze/v2/app"
 )
 
 func RegisterInterfacesAndImpls(interfaceRegistry cosmoscodectypes.InterfaceRegistry) {
