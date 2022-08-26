1120b850dd88f08523d002c7afbf094246405a15
Raumo0 <dharapko@gmail.com>
2022-08-26 16:34:12 +0200

    Akash address config added

--- pkg/codec/protobuf_codec.go
+++ pkg/codec/protobuf_codec.go
@@ -15,11 +15,32 @@ import (
 	akashapp "github.com/ovrclk/akash/app"
 )
 
+const (
+	Bech32PrefixAccAddr = "akash"
+	Bech32PrefixAccPub  = "akashpub"
+
+	Bech32PrefixValAddr = "akashvaloper"
+	Bech32PrefixValPub  = "akashvaloperpub"
+
+	Bech32PrefixConsAddr = "akashvalcons"
+	Bech32PrefixConsPub  = "akashvalconspub"
+)
+
 func RegisterInterfacesAndImpls(interfaceRegistry cosmoscodectypes.InterfaceRegistry) {
+	addressConfig()
 	impls := getMessageImplementations()
 	interfaceRegistry.RegisterImplementations((*cosmostypes.Msg)(nil), impls...)
 	akashRegisterInterfaces(interfaceRegistry)
 	registerTypes(interfaceRegistry)
+}
+
+func addressConfig() {
+	config := cosmostypes.GetConfig()
+	config.SetBech32PrefixForAccount(Bech32PrefixAccAddr, Bech32PrefixAccPub)
+	config.SetBech32PrefixForValidator(Bech32PrefixValAddr, Bech32PrefixValPub)
+	config.SetBech32PrefixForConsensusNode(Bech32PrefixConsAddr, Bech32PrefixConsPub)
+
+	config.Seal()
 }
 
 func akashRegisterInterfaces(interfaceRegistry cosmoscodectypes.InterfaceRegistry) {
