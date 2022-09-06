6e765fa277e35feda9aa4eca7af6f15b32414363
Wolthrax <wolthrax@gmail.com>
2022-09-06 12:35:38 +0300

    Fetchhub addresses fix

--- pkg/codec/protobuf_codec.go
+++ pkg/codec/protobuf_codec.go
@@ -14,11 +14,32 @@ import (
 	fetchaiapp "github.com/fetchai/fetchd/app"
 )
 
+const (
+	AccountAddressPrefix = "fetch"
+)
+
+var (
+	AccountPubKeyPrefix    = AccountAddressPrefix + "pub"
+	ValidatorAddressPrefix = AccountAddressPrefix + "valoper"
+	ValidatorPubKeyPrefix  = AccountAddressPrefix + "valoperpub"
+	ConsNodeAddressPrefix  = AccountAddressPrefix + "valcons"
+	ConsNodePubKeyPrefix   = AccountAddressPrefix + "valconspub"
+)
+
 func RegisterInterfacesAndImpls(interfaceRegistry cosmoscodectypes.InterfaceRegistry) {
+	SetConfig()
 	impls := getMessageImplementations()
 	interfaceRegistry.RegisterImplementations((*cosmostypes.Msg)(nil), impls...)
 	fetchaiRegisterInterfaces(interfaceRegistry)
 	registerTypes(interfaceRegistry)
+}
+
+func SetConfig() {
+	config := cosmostypes.GetConfig()
+	config.SetBech32PrefixForAccount(AccountAddressPrefix, AccountPubKeyPrefix)
+	config.SetBech32PrefixForValidator(ValidatorAddressPrefix, ValidatorPubKeyPrefix)
+	config.SetBech32PrefixForConsensusNode(ConsNodeAddressPrefix, ConsNodePubKeyPrefix)
+	config.Seal()
 }
 
 func fetchaiRegisterInterfaces(interfaceRegistry cosmoscodectypes.InterfaceRegistry) {
