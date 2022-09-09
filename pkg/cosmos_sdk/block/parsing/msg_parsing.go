dbec9f28744c23a38c3732ab4fef6e82c6f8dbc1
Wolthrax <wolthrax@gmail.com>
2022-09-09 16:11:11 +0300

    vidulum vanila codec fix

--- pkg/cosmos_sdk/block/parsing/msg_parsing.go
+++ pkg/cosmos_sdk/block/parsing/msg_parsing.go
@@ -13,6 +13,8 @@ import (
 	watcher "github.com/mapofzones/cosmos-watcher/pkg/types"
 	types6 "github.com/tendermint/tendermint/abci/types"
 	"log"
+	"math/big"
+	"strconv"
 )
 
 type attributeFiler struct {
@@ -243,23 +245,24 @@ func ParseIDsFromResults(txResult *types6.ResponseDeliverTx, expectedEvents []st
 }
 
 func sdkCoinsToStruct(data []sdk.Coin) []struct {
-	Amount uint64
+	Amount *big.Int
 	Coin   string
 } {
 	transformed := make([]struct {
-		Amount uint64
+		Amount *big.Int
 		Coin   string
 	}, len(data))
 
 	for i, sdkCoin := range data {
-		var amount uint64
-		if sdkCoin.Amount.IsUint64() {
-			amount = sdkCoin.Amount.Uint64()
-		} else {
-			amount = 0
+		n := new(big.Int)
+		base := 10
+		amount, ok := n.SetString(sdkCoin.Amount.String(), base)
+		if !ok {
+			log.Fatalf("Cannot unmarshal %s to bigint: error", sdkCoin.Amount)
 		}
+
 		transformed[i] = struct {
-			Amount uint64
+			Amount *big.Int
 			Coin   string
 		}{
 			Coin:   sdkCoin.Denom,
@@ -270,22 +273,27 @@ func sdkCoinsToStruct(data []sdk.Coin) []struct {
 }
 
 func packetToStruct(data transfer.FungibleTokenPacketData) []struct {
-	Amount uint64
+	Amount *big.Int
 	Coin   string
 } {
 	transformed := make([]struct {
-		Amount uint64
+		Amount *big.Int
 		Coin   string
 	}, 1)
 
-	//number, _ := strconv.ParseUint(string(data.Amount), 10, 64)
+	n := new(big.Int)
+	base := 10
+	amount, ok := n.SetString(strconv.FormatUint(data.Amount, 10), base)
+	if !ok {
+		log.Fatalf("Cannot unmarshal %s to bigint: error", strconv.FormatUint(data.Amount, 10))
+	}
+
 	transformed[0] = struct {
-		Amount uint64
+		Amount *big.Int
 		Coin   string
 	}{
-		Coin: data.Denom,
-		//Amount: number,
-		Amount: data.Amount,
+		Coin:   data.Denom,
+		Amount: amount,
 	}
 	return transformed
 }
