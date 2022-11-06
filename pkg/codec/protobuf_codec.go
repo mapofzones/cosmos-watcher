2b267f253e60fc0394b83e331e97a580bcbc1942
Raumo0 <dharapko@gmail.com>
2022-11-06 19:20:33 +0100

    Blockchain version upgrade

--- pkg/codec/protobuf_codec.go
+++ pkg/codec/protobuf_codec.go
@@ -3,15 +3,15 @@ package watcher
 import (
 	"github.com/gogo/protobuf/proto"
 
-	junoapp "github.com/CosmosContracts/juno/app"
+	junoapp "github.com/CosmosContracts/juno/v11/app"
 	cosmoscodectypes "github.com/cosmos/cosmos-sdk/codec/types"
 	cosmoscryptoed "github.com/cosmos/cosmos-sdk/crypto/keys/ed25519"
 	cosmoscryptomultisig "github.com/cosmos/cosmos-sdk/crypto/keys/multisig"
 	cosmoscryptosecp "github.com/cosmos/cosmos-sdk/crypto/keys/secp256k1"
 	cosmoscryptotypes "github.com/cosmos/cosmos-sdk/crypto/types"
 	cosmostypes "github.com/cosmos/cosmos-sdk/types"
-	ibcexported "github.com/cosmos/ibc-go/v2/modules/core/exported"
-	ibcclients "github.com/cosmos/ibc-go/v2/modules/light-clients/07-tendermint/types"
+	ibcexported "github.com/cosmos/ibc-go/v3/modules/core/exported"
+	ibcclients "github.com/cosmos/ibc-go/v3/modules/light-clients/07-tendermint/types"
 )
 
 const (
