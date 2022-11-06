d8498283c94b0aa9b00dc772b6ad5ab8a6529e80
Raumo0 <dharapko@gmail.com>
2022-11-06 19:03:31 +0100

    Blockchain version upgrade

--- pkg/codec/protobuf_codec.go
+++ pkg/codec/protobuf_codec.go
@@ -9,9 +9,9 @@ import (
 	cosmoscryptosecp "github.com/cosmos/cosmos-sdk/crypto/keys/secp256k1"
 	cosmoscryptotypes "github.com/cosmos/cosmos-sdk/crypto/types"
 	cosmostypes "github.com/cosmos/cosmos-sdk/types"
-	ibcexported "github.com/cosmos/ibc-go/v2/modules/core/exported"
-	ibcclients "github.com/cosmos/ibc-go/v2/modules/light-clients/07-tendermint/types"
-	stargazeapp "github.com/public-awesome/stargaze/v3/app"
+	ibcexported "github.com/cosmos/ibc-go/v3/modules/core/exported"
+	ibcclients "github.com/cosmos/ibc-go/v3/modules/light-clients/07-tendermint/types"
+	stargazeapp "github.com/public-awesome/stargaze/v7/app"
 )
 
 const (
