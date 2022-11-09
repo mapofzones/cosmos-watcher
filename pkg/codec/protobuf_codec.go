9de55193717b6f638fffbae2344aaa318664ebb7
Wolthrax <wolthrax@gmail.com>
2022-11-10 00:04:48 +0300

    Update AccountAddressPrefix

--- pkg/codec/protobuf_codec.go
+++ pkg/codec/protobuf_codec.go
@@ -14,11 +14,32 @@ import (
 	oraiapp "github.com/oraichain/orai/app"
 )
 
+const (
+	AccountAddressPrefix = "orai"
+)
+
+var (
+	AccountPubKeyPrefix    = AccountAddressPrefix + "pub"
+	ValidatorAddressPrefix = AccountAddressPrefix + "valoper"
+	ValidatorPubKeyPrefix  = AccountAddressPrefix + "valoperpub"
+	ConsNodeAddressPrefix  = AccountAddressPrefix + "valcons"
+	ConsNodePubKeyPrefix   = AccountAddressPrefix + "valconspub"
+)
+
 func RegisterInterfacesAndImpls(interfaceRegistry cosmoscodectypes.InterfaceRegistry) {
+	SetConfig()
 	impls := getMessageImplementations()
 	interfaceRegistry.RegisterImplementations((*cosmostypes.Msg)(nil), impls...)
 	oraiRegisterInterfaces(interfaceRegistry)
 	registerTypes(interfaceRegistry)
+}
+
+func SetConfig() {
+	config := cosmostypes.GetConfig()
+	config.SetBech32PrefixForAccount(AccountAddressPrefix, AccountPubKeyPrefix)
+	config.SetBech32PrefixForValidator(ValidatorAddressPrefix, ValidatorPubKeyPrefix)
+	config.SetBech32PrefixForConsensusNode(ConsNodeAddressPrefix, ConsNodePubKeyPrefix)
+	config.Seal()
 }
 
 func oraiRegisterInterfaces(interfaceRegistry cosmoscodectypes.InterfaceRegistry) {
