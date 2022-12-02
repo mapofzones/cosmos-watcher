901bf06d04c16d524888152497167995ae63b82f
Raumo0 <dharapko@gmail.com>
2022-12-02 01:48:18 +0100

    Blockchain upgrade

--- pkg/codec/protobuf_codec.go
+++ pkg/codec/protobuf_codec.go
@@ -10,8 +10,12 @@ import (
 	cosmoscryptosecp "github.com/cosmos/cosmos-sdk/crypto/keys/secp256k1"
 	cosmoscryptotypes "github.com/cosmos/cosmos-sdk/crypto/types"
 	cosmostypes "github.com/cosmos/cosmos-sdk/types"
-	ibcexported "github.com/cosmos/ibc-go/v2/modules/core/exported"
-	ibcclients "github.com/cosmos/ibc-go/v2/modules/light-clients/07-tendermint/types"
+	ibcexported "github.com/cosmos/ibc-go/v3/modules/core/exported"
+	ibcclients "github.com/cosmos/ibc-go/v3/modules/light-clients/07-tendermint/types"
+
+	etherapp "github.com/evmos/ethermint/app"
+	ethercodec "github.com/evmos/ethermint/crypto/codec"
+	ethertypes "github.com/evmos/ethermint/types"
 )
 
 const (
@@ -44,6 +48,9 @@ func SetConfig() {
 
 func gravityRegisterInterfaces(interfaceRegistry cosmoscodectypes.InterfaceRegistry) {
 	gravityapp.ModuleBasics.RegisterInterfaces(interfaceRegistry)
+	ethercodec.RegisterInterfaces(interfaceRegistry)
+	etherapp.ModuleBasics.RegisterInterfaces(interfaceRegistry)
+	ethertypes.RegisterInterfaces(interfaceRegistry)
 }
 
 func registerTypes(interfaceRegistry cosmoscodectypes.InterfaceRegistry) { // todo: need to nest. Maybe we can remove it. Old code
