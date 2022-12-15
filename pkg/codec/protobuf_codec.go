48a55003f61cd6eafaa3522650ade8ac887efabd
Raumo0 <dharapko@gmail.com>
2022-12-15 05:56:44 +0100

    Blockchain upgrade

--- pkg/codec/protobuf_codec.go
+++ pkg/codec/protobuf_codec.go
@@ -9,59 +9,114 @@ import (
 	cosmoscryptosecp "github.com/cosmos/cosmos-sdk/crypto/keys/secp256k1"
 	cosmoscryptotypes "github.com/cosmos/cosmos-sdk/crypto/types"
 	cosmostypes "github.com/cosmos/cosmos-sdk/types"
-	ibcexported "github.com/cosmos/ibc-go/modules/core/exported"
-	ibcclients "github.com/cosmos/ibc-go/modules/light-clients/07-tendermint/types"
+	ibcexported "github.com/cosmos/ibc-go/v5/modules/core/exported"
+	ibcclients "github.com/cosmos/ibc-go/v5/modules/light-clients/07-tendermint/types"
 	irisapp "github.com/irisnet/irishub/simapp"
 )
 
-const (
-	// Bech32ChainPrefix defines the prefix of this chain
-	Bech32ChainPrefix = "i"
+//const (
+//	// Bech32ChainPrefix defines the prefix of this chain
+//	Bech32ChainPrefix = "i"
+//
+//	// PrefixAcc is the prefix for account
+//	PrefixAcc = "a"
+//
+//	// PrefixValidator is the prefix for validator keys
+//	PrefixValidator = "v"
+//
+//	// PrefixConsensus is the prefix for consensus keys
+//	PrefixConsensus = "c"
+//
+//	// PrefixPublic is the prefix for public
+//	PrefixPublic = "p"
+//
+//	// PrefixAddress is the prefix for address
+//	PrefixAddress = "a"
+//
+//	// Bech32PrefixAccAddr defines the Bech32 prefix of an account's address
+//	Bech32PrefixAccAddr = Bech32ChainPrefix + PrefixAcc + PrefixAddress
+//	// Bech32PrefixAccPub defines the Bech32 prefix of an account's public key
+//	Bech32PrefixAccPub = Bech32ChainPrefix + PrefixAcc + PrefixPublic
+//	// Bech32PrefixValAddr defines the Bech32 prefix of a validator's operator address
+//	Bech32PrefixValAddr = Bech32ChainPrefix + PrefixValidator + PrefixAddress
+//	// Bech32PrefixValPub defines the Bech32 prefix of a validator's operator public key
+//	Bech32PrefixValPub = Bech32ChainPrefix + PrefixValidator + PrefixPublic
+//	// Bech32PrefixConsAddr defines the Bech32 prefix of a consensus node address
+//	Bech32PrefixConsAddr = Bech32ChainPrefix + PrefixConsensus + PrefixAddress
+//	// Bech32PrefixConsPub defines the Bech32 prefix of a consensus node public key
+//	Bech32PrefixConsPub = Bech32ChainPrefix + PrefixConsensus + PrefixPublic
+//)
 
-	// PrefixAcc is the prefix for account
-	PrefixAcc = "a"
+//const (
+//
+//	// Bech32ChainPrefix defines the prefix of this chain
+//	Bech32ChainPrefix = "i"
+//
+//	// PrefixAcc is the prefix for account
+//	PrefixAcc = "a"
+//
+//	// PrefixValidator is the prefix for validator keys
+//	PrefixValidator = "v"
+//
+//	// PrefixConsensus is the prefix for consensus keys
+//	PrefixConsensus = "c"
+//
+//	// PrefixPublic is the prefix for public
+//	PrefixPublic = "p"
+//
+//	// PrefixAddress is the prefix for address
+//	PrefixAddress = "a"
+//
+//	// Bech32PrefixAccAddr defines the Bech32 prefix of an account's address
+//	Bech32PrefixAccAddr = Bech32ChainPrefix + PrefixAcc + PrefixAddress
+//	// Bech32PrefixAccPub defines the Bech32 prefix of an account's public key
+//	Bech32PrefixAccPub = Bech32ChainPrefix + PrefixAcc + PrefixPublic
+//	// Bech32PrefixValAddr defines the Bech32 prefix of a validator's operator address
+//	Bech32PrefixValAddr = Bech32ChainPrefix + PrefixValidator + PrefixAddress
+//	// Bech32PrefixValPub defines the Bech32 prefix of a validator's operator public key
+//	Bech32PrefixValPub = Bech32ChainPrefix + PrefixValidator + PrefixPublic
+//	// Bech32PrefixConsAddr defines the Bech32 prefix of a consensus node address
+//	Bech32PrefixConsAddr = Bech32ChainPrefix + PrefixConsensus + PrefixAddress
+//	// Bech32PrefixConsPub defines the Bech32 prefix of a consensus node public key
+//	Bech32PrefixConsPub = Bech32ChainPrefix + PrefixConsensus + PrefixPublic
+//)
 
-	// PrefixValidator is the prefix for validator keys
-	PrefixValidator = "v"
-
-	// PrefixConsensus is the prefix for consensus keys
-	PrefixConsensus = "c"
-
-	// PrefixPublic is the prefix for public
-	PrefixPublic = "p"
-
-	// PrefixAddress is the prefix for address
-	PrefixAddress = "a"
-
-	// Bech32PrefixAccAddr defines the Bech32 prefix of an account's address
-	Bech32PrefixAccAddr = Bech32ChainPrefix + PrefixAcc + PrefixAddress
-	// Bech32PrefixAccPub defines the Bech32 prefix of an account's public key
-	Bech32PrefixAccPub = Bech32ChainPrefix + PrefixAcc + PrefixPublic
-	// Bech32PrefixValAddr defines the Bech32 prefix of a validator's operator address
-	Bech32PrefixValAddr = Bech32ChainPrefix + PrefixValidator + PrefixAddress
-	// Bech32PrefixValPub defines the Bech32 prefix of a validator's operator public key
-	Bech32PrefixValPub = Bech32ChainPrefix + PrefixValidator + PrefixPublic
-	// Bech32PrefixConsAddr defines the Bech32 prefix of a consensus node address
-	Bech32PrefixConsAddr = Bech32ChainPrefix + PrefixConsensus + PrefixAddress
-	// Bech32PrefixConsPub defines the Bech32 prefix of a consensus node public key
-	Bech32PrefixConsPub = Bech32ChainPrefix + PrefixConsensus + PrefixPublic
-)
+//const (
+//	//AccountAddressPrefix = "comdex"
+//	AccountAddressPrefix = "iaa"
+//)
+//
+//var (
+//	AccountPubKeyPrefix    = AccountAddressPrefix + "pub"
+//	ValidatorAddressPrefix = AccountAddressPrefix + "valoper"
+//	ValidatorPubKeyPrefix  = AccountAddressPrefix + "valoperpub"
+//	ConsNodeAddressPrefix  = AccountAddressPrefix + "valcons"
+//	ConsNodePubKeyPrefix   = AccountAddressPrefix + "valconspub"
+//)
 
 func RegisterInterfacesAndImpls(interfaceRegistry cosmoscodectypes.InterfaceRegistry) {
-	SetConfig()
+	//SetConfig()
 	impls := getMessageImplementations()
 	interfaceRegistry.RegisterImplementations((*cosmostypes.Msg)(nil), impls...)
 	irisRegisterInterfaces(interfaceRegistry)
 	registerTypes(interfaceRegistry)
 }
 
-func SetConfig() {
-	config := cosmostypes.GetConfig()
-	config.SetBech32PrefixForAccount(Bech32PrefixAccAddr, Bech32PrefixAccPub)
-	config.SetBech32PrefixForValidator(Bech32PrefixValAddr, Bech32PrefixValPub)
-	config.SetBech32PrefixForConsensusNode(Bech32PrefixConsAddr, Bech32PrefixConsPub)
-	config.Seal()
-}
+//func SetConfig() {
+//	config := cosmostypes.GetConfig()
+//	config.SetBech32PrefixForAccount(Bech32PrefixAccAddr, Bech32PrefixAccPub)
+//	config.SetBech32PrefixForValidator(Bech32PrefixValAddr, Bech32PrefixValPub)
+//	config.SetBech32PrefixForConsensusNode(Bech32PrefixConsAddr, Bech32PrefixConsPub)
+//	config.Seal()
+//}
+
+//func SetConfig() {
+//	config := cosmostypes.GetConfig()
+//	config.SetBech32PrefixForAccount(AccountAddressPrefix, AccountPubKeyPrefix)
+//	config.SetBech32PrefixForValidator(ValidatorAddressPrefix, ValidatorPubKeyPrefix)
+//	config.SetBech32PrefixForConsensusNode(ConsNodeAddressPrefix, ConsNodePubKeyPrefix)
+//	config.Seal()
+//}
 
 func irisRegisterInterfaces(interfaceRegistry cosmoscodectypes.InterfaceRegistry) {
 	irisapp.ModuleBasics.RegisterInterfaces(interfaceRegistry)
