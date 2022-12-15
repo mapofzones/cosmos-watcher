091c2b2fc4301fad3cf17c3caf2b001e24e90172
Raumo0 <dharapko@gmail.com>
2022-12-15 03:13:28 +0100

    Blockchain upgrade

--- pkg/codec/protobuf_codec.go
+++ pkg/codec/protobuf_codec.go
@@ -11,7 +11,7 @@ import (
 	cosmostypes "github.com/cosmos/cosmos-sdk/types"
 	ibcexported "github.com/cosmos/ibc-go/v2/modules/core/exported"
 	ibcclients "github.com/cosmos/ibc-go/v2/modules/light-clients/07-tendermint/types"
-	crescentapp "github.com/crescent-network/crescent/v2/app"
+	crescentapp "github.com/crescent-network/crescent/v3/app"
 )
 
 func RegisterInterfacesAndImpls(interfaceRegistry cosmoscodectypes.InterfaceRegistry) {
