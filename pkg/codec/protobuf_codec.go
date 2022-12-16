94d624d246cc6b99852629d382ee842b56a97ee1
Raumo0 <dharapko@gmail.com>
2022-12-16 07:45:42 +0100

    Blockchain upgrade

--- pkg/codec/protobuf_codec.go
+++ pkg/codec/protobuf_codec.go
@@ -9,9 +9,9 @@ import (
 	cosmoscryptosecp "github.com/cosmos/cosmos-sdk/crypto/keys/secp256k1"
 	cosmoscryptotypes "github.com/cosmos/cosmos-sdk/crypto/types"
 	cosmostypes "github.com/cosmos/cosmos-sdk/types"
-	ibcexported "github.com/cosmos/ibc-go/modules/core/exported"
-	ibcclients "github.com/cosmos/ibc-go/modules/light-clients/07-tendermint/types"
-	desmosapp "github.com/desmos-labs/desmos/v2/app"
+	ibcexported "github.com/cosmos/ibc-go/v3/modules/core/exported"
+	ibcclients "github.com/cosmos/ibc-go/v3/modules/light-clients/07-tendermint/types"
+	desmosapp "github.com/desmos-labs/desmos/v4/app"
 )
 
 const (
