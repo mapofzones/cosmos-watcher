421848bc7b72fceb00dc3e5cbf08275ce93a6572
Raumo0 <dharapko@gmail.com>
2023-02-16 23:12:30 +0100

    Blockchain version upgrade

--- pkg/codec/protobuf_codec.go
+++ pkg/codec/protobuf_codec.go
@@ -9,7 +9,7 @@ import (
 	cosmoscryptosecp "github.com/cosmos/cosmos-sdk/crypto/keys/secp256k1"
 	cosmoscryptotypes "github.com/cosmos/cosmos-sdk/crypto/types"
 	cosmostypes "github.com/cosmos/cosmos-sdk/types"
-	cosmosapp "github.com/cosmos/gaia/v7/app"
+	cosmosapp "github.com/cosmos/gaia/v8/app"
 	ibcexported "github.com/cosmos/ibc-go/v3/modules/core/exported"
 	ibcclients "github.com/cosmos/ibc-go/v3/modules/light-clients/07-tendermint/types"
 )
