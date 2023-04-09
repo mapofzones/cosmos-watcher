d77e0b0ae96ea86b56b8f220ff3125b9407ab5c0
Raumo0 <dharapko@gmail.com>
2023-04-10 00:01:35 +0200

    Blockchain version upgrade

--- pkg/codec/protobuf_codec.go
+++ pkg/codec/protobuf_codec.go
@@ -9,10 +9,10 @@ import (
 	cosmoscryptosecp "github.com/cosmos/cosmos-sdk/crypto/keys/secp256k1"
 	cosmoscryptotypes "github.com/cosmos/cosmos-sdk/crypto/types"
 	cosmostypes "github.com/cosmos/cosmos-sdk/types"
-	ibcexported "github.com/cosmos/ibc-go/v3/modules/core/exported"
-	ibcclients "github.com/cosmos/ibc-go/v3/modules/light-clients/07-tendermint/types"
+	ibcexported "github.com/cosmos/ibc-go/v4/modules/core/exported"
+	ibcclients "github.com/cosmos/ibc-go/v4/modules/light-clients/07-tendermint/types"
 
-	persistenceapp "github.com/persistenceOne/persistenceCore/v6/app"
+	persistenceapp "github.com/persistenceOne/persistenceCore/v7/app"
 )
 
 const (
