4f0616b426455f87e604c3b587586b04f912f9fe
Maksim Hryhoryeu <mhryhoryeu@def6.com>
2023-09-05 12:12:20 +0300

    laozi-mainnet. Update dependencies

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM bitnami/golang:1.17-debian-10 as build
+FROM bitnami/golang:1.19-debian-11 as build
 
 WORKDIR /app
 
@@ -12,11 +12,11 @@ RUN ls -la /app
 
 FROM ubuntu:latest as production
 
-RUN apt-get update && apt-get install -y curl jq coreutils
+RUN apt-get update && apt-get install -y curl jq coreutils dos2unix
 
-#COPY --from=build /app/libwasmvm.so /usr/lib/libwasmvm.so
+COPY --from=build /app/libgo_owasm.so /usr/lib/libgo_owasm.so
 COPY --from=build /app/watcher  /app/watcher
 COPY --from=build /app/scripts/run.sh /run.sh
-COPY --from=build /app/libgo_owasm.so /usr/lib/libgo_owasm.so
 
+RUN dos2unix /run.sh
 CMD ["/run.sh"]
--- pkg/cosmos_sdk/block/parsing/msg_parsing.go
+++ pkg/cosmos_sdk/block/parsing/msg_parsing.go
@@ -3,19 +3,19 @@ package cosmos
 import (
 	"encoding/json"
 	"errors"
+	types6 "github.com/tendermint/tendermint/abci/types"
+	"math/big"
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	types "github.com/cosmos/cosmos-sdk/x/bank/types"
-	transfer "github.com/cosmos/ibc-go/modules/apps/transfer/types"
-	clienttypes "github.com/cosmos/ibc-go/modules/core/02-client/types"
-	connectiontypes "github.com/cosmos/ibc-go/modules/core/03-connection/types"
-	channeltypes "github.com/cosmos/ibc-go/modules/core/04-channel/types"
-	solomachine "github.com/cosmos/ibc-go/modules/light-clients/06-solomachine/types"
-	types7 "github.com/cosmos/ibc-go/modules/light-clients/07-tendermint/types"
+	transfer "github.com/cosmos/ibc-go/v4/modules/apps/transfer/types"
+	clienttypes "github.com/cosmos/ibc-go/v4/modules/core/02-client/types"
+	connectiontypes "github.com/cosmos/ibc-go/v4/modules/core/03-connection/types"
+	channeltypes "github.com/cosmos/ibc-go/v4/modules/core/04-channel/types"
+	solomachine "github.com/cosmos/ibc-go/v4/modules/light-clients/06-solomachine/types"
+	types7 "github.com/cosmos/ibc-go/v4/modules/light-clients/07-tendermint/types"
 	watcher "github.com/mapofzones/cosmos-watcher/pkg/types"
-	types6 "github.com/tendermint/tendermint/abci/types"
 	"log"
-	"math/big"
-	"strconv"
 )
 
 type attributeFiler struct {
@@ -69,7 +69,11 @@ func parseMsg(msg sdk.Msg, txResult *types6.ResponseDeliverTx, errCode uint32) (
 		expectedEvents := []string{connectiontypes.EventTypeConnectionOpenInit}
 		attributeKeys := []string{connectiontypes.AttributeKeyConnectionID}
 		attrFiler := attributeFiler{clienttypes.AttributeKeyClientID, msg.ClientId}
-		connectionIDs := ParseIDsFromResults(txResult, expectedEvents, attributeKeys, attrFiler)
+		attrFiler2 := attributeFiler{"counterparty_client_id", msg.Counterparty.ClientId}
+		attrFiler3 := attributeFiler{"counterparty_connection_id", msg.Counterparty.ConnectionId} //??????? mb no info
+
+		connectionIDs := ParseIDsFromResults(txResult, expectedEvents, attributeKeys,
+			attrFiler, attrFiler2, attrFiler3, attributeFiler{})
 		if (len(connectionIDs) != 1 || len(connectionIDs[0]) == 0) && errCode == 0 {
 			return nil, errors.New("connectionID not found")
 		}
@@ -86,8 +90,12 @@ func parseMsg(msg sdk.Msg, txResult *types6.ResponseDeliverTx, errCode uint32) (
 		}
 		expectedEvents := []string{connectiontypes.EventTypeConnectionOpenTry}
 		attributeKeys := []string{connectiontypes.AttributeKeyConnectionID}
-		attrFiler := attributeFiler{clienttypes.AttributeKeyClientID, msg.ClientId}
-		connectionIDs := ParseIDsFromResults(txResult, expectedEvents, attributeKeys, attrFiler)
+		attrFiler1 := attributeFiler{clienttypes.AttributeKeyClientID, msg.ClientId}
+		attrFiler2 := attributeFiler{"counterparty_client_id", msg.Counterparty.ClientId}
+		attrFiler3 := attributeFiler{"counterparty_connection_id", msg.Counterparty.ConnectionId}
+
+		connectionIDs := ParseIDsFromResults(txResult, expectedEvents, attributeKeys,
+			attrFiler1, attrFiler2, attrFiler3, attributeFiler{})
 		if (len(connectionIDs) != 1 || len(connectionIDs[0]) == 0) && errCode == 0 {
 			return nil, errors.New("connectionID not found")
 		}
@@ -105,10 +113,16 @@ func parseMsg(msg sdk.Msg, txResult *types6.ResponseDeliverTx, errCode uint32) (
 		}
 		expectedEvents := []string{channeltypes.EventTypeChannelOpenInit}
 		attributeKeys := []string{channeltypes.AttributeKeyChannelID}
-		attrFiler := attributeFiler{connectiontypes.AttributeKeyConnectionID, msg.Channel.ConnectionHops[0]}
-		channelIDs := ParseIDsFromResults(txResult, expectedEvents, attributeKeys, attrFiler)
+
+		attrFiler1 := attributeFiler{channeltypes.AttributeKeyPortID, msg.PortId}
+		attrFiler2 := attributeFiler{"counterparty_port_id", msg.Channel.Counterparty.PortId}
+		attrFiler3 := attributeFiler{"counterparty_channel_id", msg.Channel.Counterparty.ChannelId}
+		attrFiler4 := attributeFiler{connectiontypes.AttributeKeyConnectionID, msg.Channel.ConnectionHops[0]}
+
+		channelIDs := ParseIDsFromResults(txResult, expectedEvents, attributeKeys,
+			attrFiler1, attrFiler2, attrFiler3, attrFiler4)
 		if (len(channelIDs) != 1 || len(channelIDs[0]) == 0) && errCode == 0 {
-			return nil, errors.New("channelID not found")
+			return nil, errors.New("MsgChannelOpenInit channelID not found")
 		}
 		return []watcher.Message{
 			watcher.CreateChannel{
@@ -124,10 +138,14 @@ func parseMsg(msg sdk.Msg, txResult *types6.ResponseDeliverTx, errCode uint32) (
 		}
 		expectedEvents := []string{channeltypes.EventTypeChannelOpenTry}
 		attributeKeys := []string{channeltypes.AttributeKeyChannelID}
-		attrFiler := attributeFiler{connectiontypes.AttributeKeyConnectionID, msg.Channel.ConnectionHops[0]}
-		channelIDs := ParseIDsFromResults(txResult, expectedEvents, attributeKeys, attrFiler)
+		attrFiler1 := attributeFiler{channeltypes.AttributeKeyPortID, msg.PortId}
+		attrFiler2 := attributeFiler{"counterparty_port_id", msg.Channel.Counterparty.PortId}
+		attrFiler3 := attributeFiler{"counterparty_channel_id", msg.Channel.Counterparty.ChannelId}
+		attrFiler4 := attributeFiler{connectiontypes.AttributeKeyConnectionID, msg.Channel.ConnectionHops[0]}
+
+		channelIDs := ParseIDsFromResults(txResult, expectedEvents, attributeKeys, attrFiler1, attrFiler2, attrFiler3, attrFiler4)
 		if len(channelIDs) != 1 || len(channelIDs[0]) == 0 {
-			return nil, errors.New("channelID not found")
+			return nil, errors.New("MsgChannelOpenTry channelID not found")
 		}
 		return []watcher.Message{
 			watcher.CreateChannel{
@@ -214,20 +232,42 @@ func ParseClientIDFromResults(txResult *types6.ResponseDeliverTx, clientId strin
 	return clientId
 }
 
-func ParseIDsFromResults(txResult *types6.ResponseDeliverTx, expectedEvents []string, attributeKeys []string, attrFiler attributeFiler) []string {
+func ParseIDsFromResults(txResult *types6.ResponseDeliverTx, expectedEvents []string, attributeKeys []string, attrFiler1 attributeFiler, attrFiler2 attributeFiler, attrFiler3 attributeFiler, attrFiler4 attributeFiler) []string {
 	var attributesValues []string
 	if txResult != nil {
 		for _, event := range txResult.Events {
 			for _, expected := range expectedEvents {
 				if event.Type == expected {
 					isCorrect := false
-					if attrFiler == (attributeFiler{}) {
+					if attrFiler1 == (attributeFiler{}) && attrFiler2 == (attributeFiler{}) && attrFiler3 == (attributeFiler{}) && attrFiler4 == (attributeFiler{}) {
 						isCorrect = true
 					} else {
+						isCorrect1 := false
+						isCorrect2 := false
+						isCorrect3 := false
+						isCorrect4 := false
 						for _, attr := range event.Attributes {
-							if attrFiler != (attributeFiler{}) &&
-								attrFiler.key == string(attr.Key) &&
-								attrFiler.value == string(attr.Value) {
+							if attrFiler1 == (attributeFiler{}) ||
+								(attrFiler1.key == string(attr.Key) &&
+									attrFiler1.value == string(attr.Value)) {
+								isCorrect1 = true
+							}
+							if attrFiler2 == (attributeFiler{}) ||
+								(attrFiler2.key == string(attr.Key) &&
+									attrFiler2.value == string(attr.Value)) {
+								isCorrect2 = true
+							}
+							if attrFiler3 == (attributeFiler{}) ||
+								(attrFiler3.key == string(attr.Key) &&
+									attrFiler3.value == string(attr.Value)) {
+								isCorrect3 = true
+							}
+							if attrFiler4 == (attributeFiler{}) ||
+								(attrFiler4.key == string(attr.Key) &&
+									attrFiler4.value == string(attr.Value)) {
+								isCorrect4 = true
+							}
+							if isCorrect1 == true && isCorrect2 == true && isCorrect3 == true && isCorrect4 == true {
 								isCorrect = true
 							}
 						}
@@ -291,12 +331,12 @@ func packetToStruct(data transfer.FungibleTokenPacketData) []struct {
 	n := new(big.Int)
 	base := 10
 	amountString := "0"
-	if len(strconv.FormatUint(data.Amount, 10)) > 0 {
-		amountString = strconv.FormatUint(data.Amount, 10)
+	if len(data.Amount) > 0 {
+		amountString = data.Amount
 	}
 	amount, ok := n.SetString(amountString, base)
 	if !ok {
-		log.Fatalf("Cannot unmarshal %s to bigint: error", strconv.FormatUint(data.Amount, 10))
+		log.Fatalf("Cannot unmarshal %s to bigint: error", data.Amount)
 	}
 
 	transformed[0] = struct {
