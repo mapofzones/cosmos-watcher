a16f28b017380ba00eaf35f0bfba01b08447c759
0xTFM <140174482+0xTFM@users.noreply.github.com>
2023-10-25 09:33:50 +0300

    add kyve

--- pkg/codec/protobuf_codec.go
+++ pkg/codec/protobuf_codec.go
@@ -9,14 +9,14 @@ import (
 	cosmoscryptosecp "github.com/cosmos/cosmos-sdk/crypto/keys/secp256k1"
 	cosmoscryptotypes "github.com/cosmos/cosmos-sdk/crypto/types"
 	cosmostypes "github.com/cosmos/cosmos-sdk/types"
-	ibcexported "github.com/cosmos/ibc-go/v4/modules/core/exported"
-	ibcclients "github.com/cosmos/ibc-go/v4/modules/light-clients/07-tendermint/types"
+	ibcexported "github.com/cosmos/ibc-go/v6/modules/core/exported"
+	ibcclients "github.com/cosmos/ibc-go/v6/modules/light-clients/07-tendermint/types"
 
-	coreum "github.com/CoreumFoundation/coreum/v2/app"
+	kyve "github.com/KYVENetwork/chain/app"
 )
 
 const (
-	AccountAddressPrefix = "core"
+	AccountAddressPrefix = "kyve"
 )
 
 var (
@@ -31,7 +31,7 @@ func RegisterInterfacesAndImpls(interfaceRegistry cosmoscodectypes.InterfaceRegi
 	addressConfig()
 	impls := getMessageImplementations()
 	interfaceRegistry.RegisterImplementations((*cosmostypes.Msg)(nil), impls...)
-	coreumRegisterInterfaces(interfaceRegistry)
+	kyveRegisterInterfaces(interfaceRegistry)
 	registerTypes(interfaceRegistry)
 }
 
@@ -43,8 +43,8 @@ func addressConfig() {
 	config.Seal()
 }
 
-func coreumRegisterInterfaces(interfaceRegistry cosmoscodectypes.InterfaceRegistry) {
-	coreum.ModuleBasics.RegisterInterfaces(interfaceRegistry)
+func kyveRegisterInterfaces(interfaceRegistry cosmoscodectypes.InterfaceRegistry) {
+	kyve.ModuleBasics.RegisterInterfaces(interfaceRegistry)
 }
 
 func registerTypes(interfaceRegistry cosmoscodectypes.InterfaceRegistry) { // todo: need to nest. Maybe we can remove it. Old code
