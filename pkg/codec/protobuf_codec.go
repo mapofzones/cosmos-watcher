e3704505378ffdbe1c2432349570fa298629102e
0xTFM <140174482+0xTFM@users.noreply.github.com>
2023-10-30 15:18:59 +0300

    add missing ed25519.PubKey

--- pkg/codec/protobuf_codec.go
+++ pkg/codec/protobuf_codec.go
@@ -4,6 +4,10 @@ import (
 	cosmoscodectypes "github.com/cosmos/cosmos-sdk/codec/types"
 	cosmostypes "github.com/cosmos/cosmos-sdk/types"
 
+	cosmoscryptoed "github.com/cosmos/cosmos-sdk/crypto/keys/ed25519"
+	cosmoscryptomultisig "github.com/cosmos/cosmos-sdk/crypto/keys/multisig"
+	cosmoscryptosecp "github.com/cosmos/cosmos-sdk/crypto/keys/secp256k1"
+	cosmoscryptotypes "github.com/cosmos/cosmos-sdk/crypto/types"
 	ibcclients "github.com/cosmos/ibc-go/v6/modules/light-clients/07-tendermint/types"
 	theapp "github.com/haqq-network/haqq/app"
 	cryptocodecs "github.com/haqq-network/haqq/crypto/codec"
@@ -16,4 +20,12 @@ func RegisterInterfacesAndImpls(interfaceRegistry cosmoscodectypes.InterfaceRegi
 	chaintypes.RegisterInterfaces(interfaceRegistry)
 	ibcclients.RegisterInterfaces(interfaceRegistry)
 	cryptocodecs.RegisterInterfaces(interfaceRegistry)
+	registerTypes(interfaceRegistry)
+}
+
+func registerTypes(interfaceRegistry cosmoscodectypes.InterfaceRegistry) {
+	interfaceRegistry.RegisterInterface("cosmos.crypto.PubKey", (*cosmoscryptotypes.PubKey)(nil))
+	interfaceRegistry.RegisterImplementations((*cosmoscryptotypes.PubKey)(nil), &cosmoscryptoed.PubKey{})
+	interfaceRegistry.RegisterImplementations((*cosmoscryptotypes.PubKey)(nil), &cosmoscryptosecp.PubKey{})
+	interfaceRegistry.RegisterImplementations((*cosmoscryptotypes.PubKey)(nil), &cosmoscryptomultisig.LegacyAminoPubKey{})
 }
