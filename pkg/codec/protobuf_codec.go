67e867eff7e8b45dc29bfa610f1e5d82fa0d4214
0XTFM <140174482+0xTFM@users.noreply.github.com>
2023-11-24 12:19:44 +0300

    add(firmachain): v0.3.5

--- pkg/codec/protobuf_codec.go
+++ pkg/codec/protobuf_codec.go
@@ -9,14 +9,14 @@ import (
 	cosmoscryptosecp "github.com/cosmos/cosmos-sdk/crypto/keys/secp256k1"
 	cosmoscryptotypes "github.com/cosmos/cosmos-sdk/crypto/types"
 	cosmostypes "github.com/cosmos/cosmos-sdk/types"
-	ibcexported "github.com/cosmos/ibc-go/v4/modules/core/exported"
-	ibcclients "github.com/cosmos/ibc-go/v4/modules/light-clients/07-tendermint/types"
+	ibcexported "github.com/cosmos/ibc-go/v3/modules/core/exported"
+	ibcclients "github.com/cosmos/ibc-go/v3/modules/light-clients/07-tendermint/types"
 
-	coreum "github.com/CoreumFoundation/coreum/v2/app"
+	firmaChain "github.com/firmachain/firmachain/app"
 )
 
 const (
-	AccountAddressPrefix = "core"
+	AccountAddressPrefix = "firma"
 )
 
 var (
@@ -31,7 +31,7 @@ func RegisterInterfacesAndImpls(interfaceRegistry cosmoscodectypes.InterfaceRegi
 	addressConfig()
 	impls := getMessageImplementations()
 	interfaceRegistry.RegisterImplementations((*cosmostypes.Msg)(nil), impls...)
-	coreumRegisterInterfaces(interfaceRegistry)
+	firmaChainRegisterInterfaces(interfaceRegistry)
 	registerTypes(interfaceRegistry)
 }
 
@@ -43,8 +43,8 @@ func addressConfig() {
 	config.Seal()
 }
 
-func coreumRegisterInterfaces(interfaceRegistry cosmoscodectypes.InterfaceRegistry) {
-	coreum.ModuleBasics.RegisterInterfaces(interfaceRegistry)
+func firmaChainRegisterInterfaces(interfaceRegistry cosmoscodectypes.InterfaceRegistry) {
+	firmaChain.ModuleBasics.RegisterInterfaces(interfaceRegistry)
 }
 
 func registerTypes(interfaceRegistry cosmoscodectypes.InterfaceRegistry) { // todo: need to nest. Maybe we can remove it. Old code
