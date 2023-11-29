1dc323746b3edcac910f29194ecb8a50b0b9005e
0XTFM <140174482+0xTFM@users.noreply.github.com>
2023-11-29 12:53:56 +0300

    add(nym): v0.43.0

--- pkg/codec/protobuf_codec.go
+++ pkg/codec/protobuf_codec.go
@@ -1,22 +1,21 @@
 package watcher
 
 import (
-	"github.com/gogo/protobuf/proto"
-
 	cosmoscodectypes "github.com/cosmos/cosmos-sdk/codec/types"
 	cosmoscryptoed "github.com/cosmos/cosmos-sdk/crypto/keys/ed25519"
 	cosmoscryptomultisig "github.com/cosmos/cosmos-sdk/crypto/keys/multisig"
 	cosmoscryptosecp "github.com/cosmos/cosmos-sdk/crypto/keys/secp256k1"
 	cosmoscryptotypes "github.com/cosmos/cosmos-sdk/crypto/types"
 	cosmostypes "github.com/cosmos/cosmos-sdk/types"
-	ibcexported "github.com/cosmos/ibc-go/v4/modules/core/exported"
-	ibcclients "github.com/cosmos/ibc-go/v4/modules/light-clients/07-tendermint/types"
+	ibcexported "github.com/cosmos/ibc-go/v7/modules/core/exported"
+	solomachine "github.com/cosmos/ibc-go/v7/modules/light-clients/06-solomachine"
+	ibcclients "github.com/cosmos/ibc-go/v7/modules/light-clients/07-tendermint"
 
-	commercio "github.com/commercionetwork/commercionetwork/app"
+	nym "github.com/nymtech/nyxd/app"
 )
 
 const (
-	AccountAddressPrefix = "did:com:"
+	AccountAddressPrefix = "n"
 )
 
 var (
@@ -29,9 +28,8 @@ var (
 
 func RegisterInterfacesAndImpls(interfaceRegistry cosmoscodectypes.InterfaceRegistry) {
 	addressConfig()
-	impls := getMessageImplementations()
-	interfaceRegistry.RegisterImplementations((*cosmostypes.Msg)(nil), impls...)
-	commercioRegisterInterfaces(interfaceRegistry)
+	interfaceRegistry.RegisterImplementations((*cosmostypes.Msg)(nil))
+	nymRegisterInterfaces(interfaceRegistry)
 	registerTypes(interfaceRegistry)
 }
 
@@ -43,8 +41,8 @@ func addressConfig() {
 	config.Seal()
 }
 
-func commercioRegisterInterfaces(interfaceRegistry cosmoscodectypes.InterfaceRegistry) {
-	commercio.ModuleBasics.RegisterInterfaces(interfaceRegistry)
+func nymRegisterInterfaces(interfaceRegistry cosmoscodectypes.InterfaceRegistry) {
+	nym.ModuleBasics.RegisterInterfaces(interfaceRegistry)
 }
 
 func registerTypes(interfaceRegistry cosmoscodectypes.InterfaceRegistry) { // todo: need to nest. Maybe we can remove it. Old code
@@ -55,20 +53,10 @@ func registerTypes(interfaceRegistry cosmoscodectypes.InterfaceRegistry) { // to
 
 	interfaceRegistry.RegisterImplementations((*ibcexported.ClientState)(nil), &ibcclients.ClientState{})
 	interfaceRegistry.RegisterImplementations((*ibcexported.ConsensusState)(nil), &ibcclients.ConsensusState{})
-	interfaceRegistry.RegisterImplementations((*ibcexported.Header)(nil), &ibcclients.Header{})
-	interfaceRegistry.RegisterImplementations((*ibcexported.Misbehaviour)(nil), &ibcclients.Misbehaviour{})
-}
-
-func getMessageImplementations() []proto.Message {
-	var impls []proto.Message
-	cosmosMessages := getCosmosMessages()
-	impls = append(impls, cosmosMessages...)
-	return impls
-}
-
-func getCosmosMessages() []proto.Message {
-	cosmosMessages := []proto.Message{
-		//&cosmostypes.ServiceMsg{}, // do i need it? cosmostypes.RegisterInterfaces don't exist ServiceMsg
-	}
-	return cosmosMessages
+	interfaceRegistry.RegisterImplementations(
+		(*ibcexported.ClientMessage)(nil),
+		&ibcclients.Header{},
+		&solomachine.Header{},
+		&solomachine.Misbehaviour{},
+	)
 }
