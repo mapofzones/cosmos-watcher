68f9188a8cea4cc536fdbb9f6c6e1d9312665e2f
0XTFM <140174482+0xTFM@users.noreply.github.com>
2023-11-30 07:51:54 +0300

    add(provenance): v1.17.0

--- pkg/codec/protobuf_codec.go
+++ pkg/codec/protobuf_codec.go
@@ -7,14 +7,14 @@ import (
 	cosmoscryptosecp "github.com/cosmos/cosmos-sdk/crypto/keys/secp256k1"
 	cosmoscryptotypes "github.com/cosmos/cosmos-sdk/crypto/types"
 	cosmostypes "github.com/cosmos/cosmos-sdk/types"
-	ibcexported "github.com/cosmos/ibc-go/v4/modules/core/exported"
-	ibcclients "github.com/cosmos/ibc-go/v4/modules/light-clients/07-tendermint/types"
+	ibcexported "github.com/cosmos/ibc-go/v6/modules/core/exported"
+	ibcclients "github.com/cosmos/ibc-go/v6/modules/light-clients/07-tendermint/types"
 
-	passage "github.com/envadiv/Passage3D/app"
+	provenance "github.com/provenance-io/provenance/app"
 )
 
 const (
-	AccountAddressPrefix = "pasg"
+	AccountAddressPrefix = "pb"
 )
 
 var (
@@ -28,7 +28,7 @@ var (
 func RegisterInterfacesAndImpls(interfaceRegistry cosmoscodectypes.InterfaceRegistry) {
 	addressConfig()
 	interfaceRegistry.RegisterImplementations((*cosmostypes.Msg)(nil))
-	passageRegisterInterfaces(interfaceRegistry)
+	provenanceRegisterInterfaces(interfaceRegistry)
 	registerTypes(interfaceRegistry)
 }
 
@@ -40,8 +40,8 @@ func addressConfig() {
 	config.Seal()
 }
 
-func passageRegisterInterfaces(interfaceRegistry cosmoscodectypes.InterfaceRegistry) {
-	passage.ModuleBasics.RegisterInterfaces(interfaceRegistry)
+func provenanceRegisterInterfaces(interfaceRegistry cosmoscodectypes.InterfaceRegistry) {
+	provenance.ModuleBasics.RegisterInterfaces(interfaceRegistry)
 }
 
 func registerTypes(interfaceRegistry cosmoscodectypes.InterfaceRegistry) { // todo: need to nest. Maybe we can remove it. Old code
