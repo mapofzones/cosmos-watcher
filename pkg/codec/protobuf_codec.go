06917d668f0a3976c9d7417c5aa1a071e68d3f8c
0XTFM <140174482+0xTFM@users.noreply.github.com>
2023-12-01 07:11:29 +0300

    add(uptick): v0.2.11

--- pkg/codec/protobuf_codec.go
+++ pkg/codec/protobuf_codec.go
@@ -9,14 +9,15 @@ import (
 	cosmoscryptosecp "github.com/cosmos/cosmos-sdk/crypto/keys/secp256k1"
 	cosmoscryptotypes "github.com/cosmos/cosmos-sdk/crypto/types"
 	cosmostypes "github.com/cosmos/cosmos-sdk/types"
-	ibcexported "github.com/cosmos/ibc-go/v4/modules/core/exported"
-	ibcclients "github.com/cosmos/ibc-go/v4/modules/light-clients/07-tendermint/types"
+	ibcexported "github.com/cosmos/ibc-go/v5/modules/core/exported"
+	ibcclients "github.com/cosmos/ibc-go/v5/modules/light-clients/07-tendermint/types"
 
-	onomy "github.com/onomyprotocol/onomy/app"
+	uptick "github.com/UptickNetwork/uptick/app"
+	ethermintcryptocodec "github.com/evmos/ethermint/crypto/codec"
 )
 
 const (
-	AccountAddressPrefix = "onomy"
+	AccountAddressPrefix = "uptick"
 )
 
 var (
@@ -31,7 +32,7 @@ func RegisterInterfacesAndImpls(interfaceRegistry cosmoscodectypes.InterfaceRegi
 	addressConfig()
 	impls := getMessageImplementations()
 	interfaceRegistry.RegisterImplementations((*cosmostypes.Msg)(nil), impls...)
-	onomyRegisterInterfaces(interfaceRegistry)
+	uptickRegisterInterfaces(interfaceRegistry)
 	registerTypes(interfaceRegistry)
 }
 
@@ -43,8 +44,9 @@ func addressConfig() {
 	config.Seal()
 }
 
-func onomyRegisterInterfaces(interfaceRegistry cosmoscodectypes.InterfaceRegistry) {
-	onomy.ModuleBasics.RegisterInterfaces(interfaceRegistry)
+func uptickRegisterInterfaces(interfaceRegistry cosmoscodectypes.InterfaceRegistry) {
+	uptick.ModuleBasics.RegisterInterfaces(interfaceRegistry)
+	ethermintcryptocodec.RegisterInterfaces(interfaceRegistry)
 }
 
 func registerTypes(interfaceRegistry cosmoscodectypes.InterfaceRegistry) { // todo: need to nest. Maybe we can remove it. Old code
